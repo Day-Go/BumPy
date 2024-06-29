d9ce3734f100f52f5749cb8bc81dee3a18cf72ea
Day-Go <lucarusso.521@gmail.com>
2024-06-29 18:38:38 +1000

    removed unused methods in physcis

--- physics.py
+++ physics.py
@@ -4,7 +4,7 @@ import numba.cuda
 from numba import cuda, float32
 
 TARGET_DENSITY = 8
-PRESSURE_MULTIPLIER = 150
+PRESSURE_MULTIPLIER = 10
 EPSILON = 1e-5
 
 @cuda.jit(device=True)
@@ -12,11 +12,6 @@ def convert_density_to_pressure(density):
     density_error = density - TARGET_DENSITY
     pressure = density_error * PRESSURE_MULTIPLIER
     return min(pressure, 0)
-
-@cuda.jit(device=True)
-def gaussian_kernel(r, h):
-    return math.exp(-(r**2) / (2 * h**2))
-
 
 @cuda.jit(device=True)
 def euclidean_distance(x_i, x_j, y_i, y_j):
@@ -31,29 +26,6 @@ def direction_to(x_i, y_i, x_j, y_j):
         dx /= distance
         dy /= distance
     return dx, dy
-
-@cuda.jit(device=True)
-def cubic_spline(q):
-    if q >= 0.5 and q <= 1:
-        base = 1 - q
-        weight = 2 * math.pow(base, 3)
-    elif q < 0.5:
-        weight = 6 * (math.pow(q, 3) - math.pow(q, 2)) + 1
-    else:
-        weight = 0
-    volume = 4 / (3 * math.pi)
-    return weight / volume
-
-@cuda.jit(device=True)
-def cubic_spline_dq(q):
-    if q >= 0.5 and q <= 1:
-        base = 1 - q
-        weight = -6 * math.pow(base, 2)
-    elif q < 0.5:
-        weight = 6 * q * (3 * q - 2) 
-    else:
-        weight = 0
-    return weight
 
 @cuda.jit(device=True)
 def spiky_kernel(r, h):
@@ -99,8 +71,8 @@ def calc_density_gradients(particles_array, h):
             dx, dy = direction_to(x_i, y_i, x_j, y_j)
             slope = spiky_kernel_gradient(r, h)
 
-            grad_x = particles_array[j]['mass'] * slope * dx / (h * r)
-            grad_y = particles_array[j]['mass'] * slope * dy / (h * r)
+            grad_x = particles_array[j]['mass'] * slope * dx / ((h * r) + EPSILON)
+            grad_y = particles_array[j]['mass'] * slope * dy / ((h * r) + EPSILON)
             particle['density_gradient'][0] += grad_x
             particle['density_gradient'][1] += grad_y
 
@@ -126,15 +98,14 @@ def calc_pressure_force(particles_array, h):
         x_j, y_j = particles_array[j]['position']
         r = math.sqrt(euclidean_distance(x_i, x_j, y_i, y_j))
         if r < h:
-            q = r / h
             dx, dy = direction_to(x_i, y_i, x_j, y_j)
-            slope = cubic_spline_dq(q)
+            slope = spiky_kernel(r, h)
 
             shared_pressure = calc_shared_pressure(particle['density'], particles_array[j]['density'])
             force_x = shared_pressure * dx * slope * particle['mass'] / (particle['density'] + EPSILON)
             force_y = shared_pressure * dy * slope * particle['mass'] / (particle['density'] + EPSILON)
-            particle['pressure_force'][0] -= force_x 
-            particle['pressure_force'][1] -= force_y 
+            particle['pressure_force'][0] += force_x 
+            particle['pressure_force'][1] += force_y 
 
 @cuda.jit(device=True)
 def update_velocity(velocity, pressure_force, density, dt):
@@ -148,7 +119,6 @@ def update_velocity(velocity, pressure_force, density, dt):
     velocity[0] += pressure_accel_x * dt
     velocity[1] += pressure_accel_y * dt
     return velocity
-
 @cuda.jit
 def update_positions(particles_array, dt, screen_width, screen_height):
     i = cuda.grid(1)
@@ -157,16 +127,14 @@ def update_positions(particles_array, dt, screen_width, screen_height):
 
     particle = particles_array[i]
 
-    # Update velocity first (assuming this updates both components)
-    update_velocity(
-        particle['velocity'], particle['pressure_force'], particle['density'], dt
-    )
+    dt_half = 0.5 * dt
+    density = particle['density'] + EPSILON  # Add epsilon to prevent division by zero
 
-    # Leapfrog integration
-    new_vx = particle['velocity'][0] + 0.5 * particle['pressure_force'][0] / particle['density'] * dt
-    new_vy = particle['velocity'][1] + 0.5 * particle['pressure_force'][1] / particle['density'] * dt
+    # First half of velocity update
+    new_vx = particle['velocity'][0] + particle['pressure_force'][0] / density * dt_half
+    new_vy = particle['velocity'][1] + particle['pressure_force'][1] / density * dt_half
 
-    # Tentative new position calculation
+    # Position update
     new_x = particle['position'][0] + new_vx * dt
     new_y = particle['position'][1] + new_vy * dt
 
@@ -186,32 +154,10 @@ def update_positions(particles_array, dt, screen_width, screen_height):
         new_y = screen_height
         new_vy *= -damping
 
-    # Update velocity again (leapfrog)
-    particle['velocity'][0] = new_vx + 0.5 * particle['pressure_force'][0] / particle['density'] * dt
-    particle['velocity'][1] = new_vy + 0.5 * particle['pressure_force'][1] / particle['density'] * dt
-    
-    # Update position with potentially modified velocity
+    # Update position
     particle['position'][0] = new_x
     particle['position'][1] = new_y
 
-def gaussian_kernel_np(r, h):
-    return np.exp(-(r**2) / (2 * h**2))
-
-
-def smoothing_kernel_np(positions, masses, densities, field_values, h):
-    n = positions.shape[0]
-    outputs = np.zeros(n)
-
-    # Compute all pairwise distances
-    for i in range(n):
-        r = np.linalg.norm(positions[i] - positions, axis=1)
-        weights = gaussian_kernel_np(r, h) * masses / densities
-        total_weight = np.sum(weights)
-        weighted_sum = np.sum(weights * field_values)
-
-        if total_weight > 0:
-            outputs[i] = weighted_sum / total_weight
-        else:
-            outputs[i] = 0
-
-    return outputs
+    # Second half of velocity update
+    particle['velocity'][0] = new_vx + particle['pressure_force'][0] / density * dt_half
+    particle['velocity'][1] = new_vy + particle['pressure_force'][1] / density * dt_half
